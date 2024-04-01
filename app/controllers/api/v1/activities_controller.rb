b1771f28d0651834ff8192bc4c49dbffc83643fa
Lorenna Vieira <lorennasv@gmail.com>
2024-04-01 22:14:55 +0100

    improved controllers to format the date and time to store it correctly in the database, finished setting up categories controller, created a time_formatter concern to use for formating the time

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Api::V1::ActivitiesController < ApplicationController
+  include TimeFormatter
+
   before_action :authenticate_user!
   def index
     @activities = current_user.activities
@@ -8,7 +10,7 @@ class Api::V1::ActivitiesController < ApplicationController
   end
 
   def create
-    activity = current_user.activities.build(activity_params)
+    activity = build_activity
     return api_response(activity, :created) if activity.save
 
     api_response(activity.errors, :unprocessable_entity)
@@ -17,6 +19,18 @@ class Api::V1::ActivitiesController < ApplicationController
   private
 
   def activity_params
-    params.require(:activity).permit(:name, :description, :date, :start_time, :end_time)
+    params.require(:activity).permit(:name, :description, :date, :start_time, :finish_time, :category_id)
+  end
+
+  def build_activity
+    date = activity_params[:date]
+    current_user.activities.build(
+      name: activity_params[:name],
+      description: activity_params[:description],
+      date:,
+      start_time: format_time(date, activity_params[:start_time]),
+      finish_time: format_time(date, activity_params[:finish_time]),
+      category_id: activity_params[:category_id]
+    )
   end
 end
