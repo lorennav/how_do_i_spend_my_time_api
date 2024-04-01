3d9d76ffb88d91c76be4c96a58626b5b86e215ac
Lorenna Vieira <lorennasv@gmail.com>
2024-04-01 22:11:06 +0100

    fixed migrations to change type of start_time and finish_time to be datetime, in order to manipulate the data better, improved the order of creating users, categories and activities according to their relations

--- db/migrate/20240401200809_create_categories.rb
+++ db/migrate/20240401200809_create_categories.rb
@@ -2,6 +2,7 @@ class CreateCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :categories do |t|
       t.string :name
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
