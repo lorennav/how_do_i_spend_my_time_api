e19b5425d3e9619772a4289a4c0254f70157527e
Lorenna Vieira <lorennasv@gmail.com>
2024-04-02 22:50:33 +0100

    updated specs to update an activity

--- spec/requests/api/v1/activities_spec.rb
+++ spec/requests/api/v1/activities_spec.rb
@@ -1,7 +1,7 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/activities', type: :request do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, :with_activities_and_categories) }
   let(:headers) { { 'Accept' => 'application/json', 'Content-Type' => 'application/json' } }
   let(:auth_headers) { Devise::JWT::TestHelpers.auth_headers(headers, user) }
   let(:Authorization) { auth_headers['Authorization'] }
@@ -21,25 +21,29 @@ RSpec.describe 'api/v1/activities', type: :request do
       tags 'Activities'
       consumes 'application/json'
       security [Bearer: {}]
-      parameter name: :activity, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string },
-          description: { type: :string },
-          date: { type: :string },
-          start_time: { type: :string },
-          finish_time: { type: :string },
-          category: { type: :object }
-        },
-        required: %w[name date start_time finish_time]
-      }
+      parameter name: :activity, in: :body, schema: { '$ref' => '#/components/schemas/activity' }
       response '201', 'activity created' do
-        let(:activity) { { name: 'Study', date: '2021-09-01', start_time: '10:00:00', finish_time: '12:00:00' } }
+        let(:activity) { { name: 'Study', date: '2021-09-01', start_time: '10:00', finish_time: '12:00' } }
         run_test!
       end
 
       response '422', 'invalid request' do
         let(:activity) { { name: 'Study' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/activities/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+    put 'Updates an activity' do
+      tags 'Activities'
+      consumes 'application/json'
+      security [Bearer: {}]
+      parameter name: :activity, in: :body, schema: { '$ref' => '#/components/schemas/activity' }
+      response '200', 'activity updated' do
+        let(:id) { user.activities.first&.id }
+        let(:activity) { { name: 'New activity' } }
         run_test!
       end
     end
